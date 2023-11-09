fa0c86bf837ef56716037b8264848d0e4e36e820
zhangkun <zk1634@163.com>
2023-11-09 16:32:19 +0800

    静态页面上传

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,6 +14,7 @@ const isProd = process.env.NODE_ENV === 'production'
 export default defineUserConfig({
     // set site base to default value
     base: '/',
+    dest: ".site",
     locales: {
         '/': {
             lang: 'zh-CN',
